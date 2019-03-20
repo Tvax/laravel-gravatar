1f1ccf97c07da7e546e9a16bcbed41542d16c8c7
Tvax <19556689+Tvax@users.noreply.github.com>
2019-03-20 18:30:48 +0100

    Create app models

--- database/migrations/2019_03_20_160629_create_avatars_table.php
+++ database/migrations/2019_03_20_160629_create_avatars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAvatarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('avatars', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+//            $table->string('uri')->unique();
+            $table->string('uri');
+            $table->boolean('default');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('avatars');
     }
 }
