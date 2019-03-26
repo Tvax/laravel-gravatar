ee6fc29e7355eab05a81053c5af1c656b2e71166
Tvax <19556689+Tvax@users.noreply.github.com>
2019-03-26 14:36:33 +0100

    Add avatar api route

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,11 +7,55 @@ use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ApiController extends Controller{
     public function config(){
-        return response()->json([
+        return response()->json($this->getConfig());
+    }
+
+    public function avatar(Request $request){
+        validateEmail($request);
+        //TODO get avatar picture
+        if($avatar != null){
+            return $this->sendJson($avatar);
+        }
+        //if unknown adress
+        return $this->sendJson($this->getError404());
+    }
+
+    private function getConfig(){
+        return [
+            'status' => 'success',
             'version' => '1',
             'size' => '50',
             'defaultSize' => '50',
             'format' => 'jpg',
+        ];
+    }
+
+    public function error(){
+        return $this->sendJson($this->getError404());
+    }
+
+    private function getAvatar($avatar){
+        return [
+            'status' => 'success',
+            'avatar' => $avatar,
+        ];
+    }
+
+    private function getError404(){
+        return [
+            'status' => 'error',
+            'code' => '404',
+            'message' => 'Email not found',
+        ];
+    }
+
+    private function sendJson($data){
+        return response()->json($data);
+    }
+
+    private function validateEmail($request){
+        $request->validate([
+            'email' => 'required|email'
         ]);
     }
 }
